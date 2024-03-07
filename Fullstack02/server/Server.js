f1cf09485034a270b99d1004f8f1b9c384b1f20a
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-07 20:43:46 +0530

    server building done.

---
+++ Fullstack02/server/Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+const cors = require('cors');
+const port = 3000 || process.env.port
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get( '/', (req, res) => {
+    res.send("mai ke chodo")
+} );
+
+app.listen(port , ()=>{
+    console.log(`Server is running on ${port}`);
+})
