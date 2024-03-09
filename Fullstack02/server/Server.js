36fc4d5574a28ea8a9ee9af9ac1d35058e5472ce
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-09 19:09:32 +0530

    update todo done foe adding new documents to my database.

--- Fullstack02/server/Server.js
+++ Fullstack02/server/Server.js
@@ -3,15 +3,28 @@ const app = express();
 const mongoose = require('mongoose');
 var bodyParser = require("body-parser");
 const cors = require('cors');
+const todoModel = require("./Todo");
+
 const port = 3000 || process.env.port
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("hello guys")
-});
+async function updateTodo (req, res) {
+    const todo = new todoModel({
+        id: Math.random().toString(36).substring(2,9),
+        title: req.body.title,
+        description: req.body.description,
+    })
+    try{const result = await todo.save();
+        res.status(201).json(result);}
+    catch(error){res.status(400).json({message:"Error in Todo creation"})}
+}
 
-app.listen(port , () => {
+
+app.get("/todo", findTodo);
+
+
+app.listen(port,() => {
     console.log(`Server is running on ${port}`);
 })
--- Fullstack02/server/Todo.js
+++ Fullstack02/server/Todo.js
@@ -8,7 +8,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/tododatabase");
 const todoSchema = new mongoose.Schema({
     _id: {
         type: String,
-        default: () => Math.random().toString(36).substr(2,9)
+        default: () => Math.random().toString(36).substring(2,9)
     },
     title: String,
     description: String,
