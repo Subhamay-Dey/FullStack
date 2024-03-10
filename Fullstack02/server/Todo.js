9d614af6d161d897528b51fd24c981f772226ec1
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-10 23:31:09 +0530

    server.js and todo.js updated

--- Fullstack02/server/Todo.js
+++ Fullstack02/server/Todo.js
@@ -1,24 +1,9 @@
-// const mongoose = require("mongoose");
-
-// mongoose.connect("mongodb://127.0.0.1:27017/tododb");
-
-// const todoSchema = mongoose.Schema({
-//     _id: {
-//         type: String,
-//         default: () => Math.random().toString(36).substr(2,9)
-//     },
-//     title: String,
-//     description: String
-// });
-
-// module.exports= mongoose.model("todo", todoSchema);
-
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/todoDB");
+mongoose.connect("mongodb://127.0.0.1:27017/tododb");
 
 const todoSchema = mongoose.Schema({
-    _id:{
+    _id: {
         type: String,
         default: () => Math.random().toString(36).substr(2,9)
     },
@@ -26,4 +11,4 @@ const todoSchema = mongoose.Schema({
     description: String
 });
 
-module.exports = mongoose.model("Todo", todoSchema);
+module.exports= mongoose.model("todo", todoSchema);
