009bd025035e5e07dd43b4bc044afb9701e68eff
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-10 22:25:34 +0530

    backend connected to frontend.

--- Fullstack02/client/src/components/Home.jsx
+++ Fullstack02/client/src/components/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import axios, { Axios } from 'axios'
+import { useEffect } from 'react';
+import { text } from 'body-parser';
+import { response } from 'express';
+import { set } from 'mongoose';
 
 const Home = () => {
+
+    const [alltodo, setAlltodo] = React.useState([]);
+    const [text, setText] = React.useState('');
+    const [description, setDescription] = React.useState('');
+
+    const getdata = async() => {
+        const response = await Axios.get('http://localhost:8080/todo');
+        setAlltodo(response.data);
+    }
+
+    
+
+
+
   return (
-    <div>Home</div>
+    <>
+
+    </>
   )
 }
 
