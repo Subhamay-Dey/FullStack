170e88afbea4556a7dc4ce0129b0edf58290ef8c
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-29 22:07:44 +0530

    "Added ejs dependency, updated index.js to render index.ejs, and created a new index.ejs and also used path.join to access directories and uses fs of express js to read directory and used it in our server component.."

--- Fullstack03/index.js
+++ Fullstack03/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
+const fs = require('fs');
 
 app.set('view engine', "ejs");
 app.use(express.json());
@@ -8,7 +9,13 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    fs.readdir(`./documents`, function(err, documents) {
+        res.render("index", {documents: documents});
+    })
+})
+
+app.post("/create", function(req, res) {
+    console.log(req.body);
 })
 
 app.listen(3000);
