68c5e6de65d0868b38936996a472c5d74fa7567a
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-29 23:42:11 +0530

    the ejs file input values are accessed by the fs and saved the task and then redirect it to the home page.

--- Fullstack03/index.js
+++ Fullstack03/index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 app.set('view engine', "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(req, res) {
     fs.readdir(`./documents`, function(err, documents) {
@@ -15,7 +15,9 @@ app.get("/", function(req, res) {
 })
 
 app.post("/create", function(req, res) {
-    console.log(req.body);
+    fs.writeFile(`./documents/${req.body.title.split(' ').join('')}.txt`, req,body.details, function(err) {
+        res.redirect("/")
+    })
 })
 
 app.listen(3000);
